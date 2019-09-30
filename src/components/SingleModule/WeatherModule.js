794612b94e24d67c622c49da2973954e1f214abf
GCaras <gcaras0@gmail.com>
2019-09-30 12:47:36 -0400

    added components and file structure

---
+++ src/components/SingleModule/WeatherModule.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class WeatherModule extends Component {
+    render() {
+        return(
+            <div>
+                <nav>
+                    <p>WeatherModuleTEST</p>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default WeatherModule
