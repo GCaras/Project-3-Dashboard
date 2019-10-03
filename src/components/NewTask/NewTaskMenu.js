c72cc5189b722ad3feb59942a02f2b96937d97cc
Torrey Thomas <Torreyisgone@gmail.com>
2019-10-03 13:19:08 -0400

    I've added three div wrappers in the new task menu.js file to create styling for the new task boxes

--- src/components/NewTask/NewTaskMenu.js
+++ src/components/NewTask/NewTaskMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import "./NewTaskMenu.css"
 
 class NewTaskMenu extends Component {
     render() {
@@ -7,13 +8,19 @@ class NewTaskMenu extends Component {
             <div>
                 <section>
                     <Link to="/NewToDo/">
+                        <div className="boxImage1">
                         <div>New Do</div>
+                        </div>
                     </Link>
                     <Link to="/NewToRead/">
+                    <div className="boxImage2">
                         <div>New Read</div>
+                        </div>
                     </Link>
                     <Link to="/NewToWatch/">
+                    <div className="boxImage2">
                         <div>New Watch</div>
+                        </div>
                     </Link>
                 </section>
             </div>
